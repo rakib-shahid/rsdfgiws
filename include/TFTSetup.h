5cfb5b58be21c305643d6e630c0603f32790ee8f
rakib-shahid <rakibshahid1298@gmail.com>
2024-10-22 11:39:56 -0400

    initial tftsetup

--- include/TFTSetup.h
+++ include/TFTSetup.h
@@ -3,11 +3,15 @@
 #define TFTSETUP_H
 
 #include <TFT_eSPI.h>
+#include <Wire.h>
+#include <Adafruit_FT6206.h>
 
 // Declare a global TFT_eSPI object
 extern TFT_eSPI tft;
+extern Adafruit_FT6206 touch;
 
 // Function to initialize the TFT display
 void initializeTFT();
+void initializeTouch();
 
 #endif // TFTSETUP_H
