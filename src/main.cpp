1d077bb64ee9363fc1f79615d0117a4c2d8abb36
rakib-shahid <rakibshahid1298@gmail.com>
2024-10-23 23:01:34 -0400

    horizontal

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,57 @@
 #include "TFTSetup.h"
 
-// Initialize touch controller
-
-// Define the LED pin
 const int ledPin = 27;
 
 void setup()
 {
-    Serial.begin(115200); // Start serial communication
+    Serial.begin(115200);
     initializeTFT();
     initializeTouch();
 
-    // // Reset the touch controller
-    // pinMode(16, OUTPUT);
-    // digitalWrite(16, LOW); // Hold reset
-    // delay(100);
-    // digitalWrite(16, HIGH); // Release reset
-
-    // Initialize the LED pin as OUTPUT
     pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, LOW); // Start with LED off
+    digitalWrite(ledPin, LOW);
+}
+
+void mapTouchCoordinates(int &x, int &y)
+{
+    int temp;
+    switch (tft.getRotation())
+    {
+    case 1:
+        temp = x;
+        x = y;
+        y = tft.height() - temp;
+        break;
+    case 2:
+        x = tft.height() - x;
+        y = tft.width() - y;
+        break;
+    case 3:
+        temp = x;
+        x = tft.width() - y;
+        y = temp;
+        break;
+    default:
+        break;
+    }
 }
 
 void loop()
 {
-
-    // Check if the screen is being touched
     if (touch.touched())
     {
-        TS_Point p = touch.getPoint(); // Get the touch coordinates
-
-        // Map the touch coordinates to the screen size
-        int x = map(p.x, 0, tft_width, 0, tft.width());
-        int y = map(p.y, 0, tft_height, 0, tft.height());
-
-        // Turn on the LED while touching the screen
-        Serial.println("LED ON");
+        TS_Point p = touch.getPoint();
+        int x = p.x;
+        int y = p.y;
+        mapTouchCoordinates(x, y);
+        // Serial.println("LED ON");
         digitalWrite(ledPin, HIGH);
-
-        // Print the touch coordinates to Serial Monitor
         Serial.printf("Touch at: %d, %d\n", x, y);
-
-        // Draw a small white circle at the touch point
         tft.fillCircle(x, y, 5, TFT_WHITE);
     }
     else
     {
-        // Turn off the LED when no touch is detected
         digitalWrite(ledPin, LOW);
-        Serial.println("LED OFF");
+        // Serial.println("LED OFF");
     }
 }
